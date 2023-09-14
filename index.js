b0f407a13540ab2051354452f5b996f00362021b
viktorevstigneev <viktor_evstignnev14@bk.ru>
2023-09-15 00:30:26 +0300

    fix 120 000

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const start = async () => {
     const chatId = msg.message.chat.id;
 
     if (data === "/again") {
-         setInterval(intervalFunc, 10000);
+         setInterval(intervalFunc, 120000);
     }
   });
 };
